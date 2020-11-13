d7a2c0653156ac5d74adaeb97b0c51746a7daae3
Paul <isakura313@gmail.com>
2020-11-13 20:39:56 +0300

    adsasd

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrapper">
+    <Header />
   <div class="columns">
     <div class="column">
       <h1 class="title is-size-3 has-text-centered" v-text="title"> </h1>
@@ -12,11 +13,13 @@
 
 <script>
 import Card from './components/Card.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     Card,
+    Header,
   },
   data() {
     return {
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img v-bind:src="logo_src" width="200">
+      </a>
+      <a role="button" class="navbar-burger burger" aria-label="menu"
+         aria-expanded="false" data-target="navbar_ecommerce">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbar_ecommerce" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Книги
+        </a>
+
+        <a class="navbar-item">
+          Видеокарты
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <input type="text" class="input">
+        </div>
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Поиск</strong>
+            </a>
+            <a class="button is-light">
+              Корзина
+              <ion-icon name="cart-outline" size="large"></ion-icon>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Header.vue',
+  data() {
+    return {
+      logo_src: 'https://cdn1.ozone.ru/s3/cms/8c/t22/ozon_logo_bf.svg',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
